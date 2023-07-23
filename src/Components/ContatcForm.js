029129dc20320c549f61b4b8ee52939d5ec4e766
KartikChawla09 <kartik.chawla.ug21@nsut.ac.in>
2023-07-24 00:15:36 +0530

    fixed form and map

--- src/Components/ContatcForm.js
+++ src/Components/ContatcForm.js
@@ -2,40 +2,8 @@ import React from "react";
 import "./ContactForm.css";
 const ContatcForm = () => {
   return (
-    <div>
+    <div className="contact-box-main">
       <div className="contact-form">
-        {/* <div className="first-container">
-          <div className="info-container">
-            <div>
-              <h4 className="contact-us-contact-info">Contact Info</h4>
-              <h3>Address</h3>
-              <p>Mada Center 8th floor, 379 Hudson St, New York, NY 10018 US</p>
-            </div>
-            <div>
-              <h3>Lets Talk</h3>
-              <p>+1 800 1236879</p>
-            </div>
-            <div>
-              <h3>General Support</h3>
-              <p>contact@example.com</p>
-            </div>
-          </div>
-        </div> */}
-        <div className="info-container">
-          <div>
-            <h4 className="contact-us-contact-info">Contact Info</h4>
-            <h3>Address</h3>
-            <p>Mada Center 8th floor, 379 Hudson St, New York, NY 10018 US</p>
-          </div>
-          <div>
-            <h3>Lets Talk</h3>
-            <p>+1 800 1236879</p>
-          </div>
-          <div>
-            <h3>General Support</h3>
-            <p>contact@example.com</p>
-          </div>
-        </div>
         <div className="second-container">
           <h2>Send Us A Message</h2>
           <form>
@@ -77,20 +45,29 @@ const ContatcForm = () => {
           </form>
         </div>
       </div>
-      <div className="conatct-map">
-        <div>
-          <iframe
-            width="100%"
-            height="600"
-            frameborder="0"
-            marginheight="0"
-            marginwidth="0"
-            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Canada+(Livespace)&amp;t=k&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-          >
-            <a href="https://www.maps.ie/population/">
-              Population calculator map
-            </a>
-          </iframe>
+      <div className="contact-map">
+        <iframe
+          width="100%"
+          height="600"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Canada+(Livespace)&amp;t=k&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        ></iframe>
+        <div className="info-container">
+          <div>
+            <h4 className="contact-us-contact-info">Contact Info</h4>
+            <h3>Address</h3>
+            <p>Mada Center 8th floor, 379 Hudson St, New York, NY 10018 US</p>
+          </div>
+          <div>
+            <h3>Lets Talk</h3>
+            <p>+1 800 1236879</p>
+          </div>
+          <div>
+            <h3>General Support</h3>
+            <p>contact@example.com</p>
+          </div>
         </div>
       </div>
     </div>
