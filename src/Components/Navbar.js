06f4ab05ccf4e9d075227217d6ed5bb9f34e3606
KartikChawla09 <kartik.chawla.ug21@nsut.ac.in>
2023-07-24 13:27:15 +0530

    hamburger fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Navbar.css";
 import CompanyLogo from "../assets/company.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    // Add event listener to close the menu when clicking outside of it
+    const handleOutsideClick = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
 
   const handleParagraphClick = () => {
     setMenuOpen(false);
   };
 
   return (
-    <div className="box">
+    <div className="box" ref={menuRef}>
       <div className="navbar-logo">
         <img src={CompanyLogo} alt="logo" />
       </div>
